56f2840f6c34c460566fa6525e37284d53cf7d8b
dara <dara@spbshop.com>
2018-09-28 20:50:08 +0700

    add more function quick get sd and hd
    const video = 'https://www.facebook.com/9gag/videos/10155721204506840/';
    
    //Quick get all
    getDown(video).then(vid => {
    	console.log(vid)
    	// => {
    	//   sd: 'https://z-p3-video.fpnh5-2.fna.fbcdn.net/v/...80e9a6ca6bf93aff3f361031580cd0e7&oe=5BAE6793',
    	//   hd: 'https://z-p3-scontent.fpnh5-2.fna.fbcdn.net/v/...fe7c4ac5a6035beadf717e&oe=5C52EA14'
    	// }
    });

--- index.js
+++ index.js
@@ -4,6 +4,44 @@ const got = require('got');
 
 const msg = 'Either the video is deleted or it\'s not shared publicly!';
 
+const getDown = link => {
+	return got(link).then(res => {
+		let sd, hd = '';
+		try {
+			sd = data.split('sd_src:"')[1].split('",hd_tag')[0];
+		} catch (e) {
+			sd = false;
+		}
+
+		try {
+			hd = data.split('hd_src:"')[1].split('",sd_src:"')[0];
+		} catch (e) {
+			hd = false;
+		}
+
+		return {
+			sd: sd,
+			hd: hd
+		}
+
+		if (hd || sd) {
+			return {
+				sd: sd,
+				hd: hd
+			}
+		}
+		return err.message;
+	}).catch(err => {
+		if (err) {
+			err.message = msg;
+		}
+		return err.message;
+	});
+
+
+	return false;
+};
+
 const lowResolution = link => {
 	return got(link).then(res => {
 		const link = res.body.split('sd_src:"')[1].split('",hd_tag')[0];
